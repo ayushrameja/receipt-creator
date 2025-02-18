8e471ce967bd9aa386afe3e933c790c6efbd267d
Ayush Rameja <ayushrameja@Ayushs-MacBook-Pro.local>
2025-02-18 09:53:10 +0530

    Updated automatic calculation

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,26 @@ import { IReceipt } from "./interfaces/IReceipt";
 
 const App: React.FC = () => {
   const [receiptData, setReceiptData] = useState<IReceipt>({
-    date: "12/12/25",
-    customerName: "Ayush Rameja",
-    total: 250,
-    tripCharge: 100,
-    rounding: 10,
+    date: "December 11, 2024",
+    customerName: "Chitwan",
+    linkRedirect: "https://www.uber.com",
+    total: 55.0,
+    tripCharge: 54.82,
+    rounding: 0.18,
+    gstAmount: 2.9,
     paymentMethod: "Cash",
-    paymentTime: "12:15 PM",
-    driverName: "John",
-    licensePlate: "XX XX XX XXXX",
-    vehicleType: "Sedan",
-    distance: "0",
-    duration: "0",
-    pickupTime: "00:00",
-    dropTime: "00:00",
-    pickupLocation: "Pickup Location",
-    dropLocation: "Drop Location",
-    gstAmount: 0,
+    paymentTime: "12/11/24 11:02 AM",
+    driverName: "SUBODH KUMAR SINGH",
+    licensePlate: "DL3SFC8221",
+    vehicleType: "Moto Saver",
+    distance: "6.60",
+    duration: "17",
+    pickupTime: "10:44 AM",
+    pickupLocation:
+      "A31, Block A, Industrial Area, Sector 62, Noida, Uttar Pradesh 201309, India",
+    dropTime: "11:02 AM",
+    dropLocation:
+      "Qa Infotech, Block A, Sector 68, Noida, Uttar Pradesh 201307, India",
   });
 
   return (
